b2991fd7e73faa380ccb9d7193f92bdbcea474a0
camilo-camargo <camilocamargo49@gmail.com>
2024-03-14 23:43:45 -0500

    - Add thin mode. - Refactor functions to utils

--- main.py
+++ main.py
@@ -1,124 +1,6 @@
 import utils
-import math
 import numpy as np
 import bisect
-
-
-def lerp(x0, y0, x1, y1, x):
-    return y0 + (x-x0)*(y1-y0)/(x1-x0)
-
-
-def ambient_factor(a3, b3, km, s, sopt):
-    return 10 ** (
-        a3 + (b3 - a3) / (1 + np.exp(np.log(-b3 / a3) + km * (s - sopt)))
-    )
-
-
-def tetha_opt(wopt, pdmax):
-    return ((wopt / 100) * pdmax)/1000
-
-
-def c_swcc(m2, bf):
-    return math.log10(m2**1.15) - (1 - (1 / bf))
-
-
-def cf_swcc(c, d10):
-    return 0.26 * math.exp(0.758 * c) + 1.4 * d10
-
-
-def bf_swcc(b):
-    if 0.936 * b - 3.8 < 0.3:
-        return 0.3
-    elif 0.936 * b - 3.8 >= 4:
-        return 4
-    else:
-        return 0.936 * b - 3.8
-
-
-def af_swcc(a, p200):
-    if (1.14 * a - 0.5) < 0:
-        return 1
-    elif (1.14 * a - 0.5) > 0 and (1.14 * a - 0.5) < 1:
-        return 2.25 * p200**0.5 + 5
-    else:
-        return 1.14 * a - 0.5
-
-
-def b_swcc(p200, d90, d10, d0, m1):
-    return (5.39 - 0.29 * math.log(
-        p200 * (d90 / d10)
-    ) + 3 * d0**0.57 + 0.021 * p200**1.19) * m1**0.1
-
-
-def a_swcc(d20, p200, d30, d100):
-    return -2.79 - 14.1 * math.log10(d20) - 1.9 * 10**-6 *\
-        p200**4.34 + 7 * math.log10(d30) + 0.055 * d100
-
-
-def d_100(m1, d60):
-    return 10 ** ((40/m1) + math.log10(d60))
-
-
-def d_0(m2, d30):
-    return 10 ** ((-30/m2) + math.log10(d30))
-
-
-def m_1(d90, d60):
-    return 30 / (math.log10(d90) - math.log10(d60))
-
-
-def m_2(d30, d10):
-    return 20 / (math.log10(d30) - math.log10(d10))
-
-
-def volumetry_humedity_saturated(pdmax, gs):
-    return 1 - pdmax / (1000*gs)
-
-
-def volumetry_humedity(ch, osat, hm, af, bf, cf):
-    return ch * (osat/(np.log(np.exp(1) + (hm/af) ** bf)**cf))
-
-
-def monthly_heat_index(average_temperature_celsius):
-    return (average_temperature_celsius/5) ** 1.514
-
-
-def ept_without_correction(
-    temp_celsius,
-    anual_heat,
-    anual_temp_3rd_poly
-):
-    return 16 * (10 * temp_celsius / anual_heat) ** anual_temp_3rd_poly
-
-
-def ept_correction(ept_unadjust, sunshine_hours, monthly_days):
-    return ept_unadjust * (sunshine_hours/12)*(monthly_days/30)
-
-
-def thornthwaite_moisture_index(ept_adjusted, precipitation,):
-    return 75 * (precipitation / ept_adjusted - 1) + 10
-
-
-def matric_suction_plastic(a, b, y, s, tmi):
-    return a ** (math.e ** (b / (tmi+y)) + s)
-
-
-def matric_suction_no_plastic(a, b, y, tmi):
-    return a + math.e ** (b+y*(tmi+101))
-
-
-def third_degree_polynomial(anual_temp):
-    a = (675 * 10**-9 * anual_temp ** 3)
-    b = (771 * 10**-7 * anual_temp ** 2)
-    c = (1792 * 10**-5 * anual_temp)
-    d = 0.49239
-
-    return a - b + c + d
-
-
-def adjust_factor(hm, hr):
-    return (1 - (np.log(1 + (hm / hr)) / (np.log(1 + (10 ** 6 / hr)))))
-
 
 SIEVES_SIZES_IN_MM = {
     "2": 50.8,
@@ -176,12 +58,40 @@ TMI_PLASTIC = {
     16: {'a': 23.564, 'b': 8.283, 'y': -0.05218}
 }
 
-specific_gravity = 2.611
-plasticity_index = 7.12
-california_bearing_ratio = 100
-maximum_dry_density = 2024
-optimum_moisture_content = 8.500
-percentage_passing_n200 = sieves_passing['No200']
+TMI_NO_PLASTIC = {
+    0: {"a": 0.3, "b": 419.07, "y": 133.5, "s": 15},
+    1: {"a": 0.3, "b": 521.5, "y": 137.3, "s": 16},
+    2: {"a": 0.3, "b": 521.5, "y": 137.3, "s": 16},
+    3: {"a": 0.3, "b": 663.5, "y": 142.5, "s": 17.5},
+    4: {"a": 0.3, "b": 801, "y": 147.6, "s": 25},
+    5: {"a": 0.3, "b": 975, "y": 152.5, "s": 32},
+    6: {"a": 0.3, "b": 1171.2, "y": 157.5, "s": 27.8}
+}
+
+
+mode = 'thin'  # or thick
+
+# Thick
+specific_gravity = 2.611  # gs
+plasticity_index = 7.12  # ip
+california_bearing_ratio = 100  # cbr
+maximum_dry_density = 2024  # pdmax
+optimum_moisture_content = 8.500  # wopt
+
+# Thin
+specific_gravity = 2.736  # gs
+plasticity_index = 19  # ip
+california_bearing_ratio = 30  # cbr
+maximum_dry_density = 1240  # pdmax
+optimum_moisture_content = 38  # wopt
+thin_n200 = 58
+
+if mode == 'thin':
+    p200 = thin_n200
+    wpi = (p200 / 100) * plasticity_index
+else:
+    p200 = sieves_passing['No200']
+
 
 precipitation_mm = np.array(
     [19.03, 21.51, 70.85, 81.81, 87.84, 44.50,
@@ -192,85 +102,151 @@ temp_celsius = np.array(
      12.44, 12.54, 13.07, 13.48, 13.93, 13.55]
 )
 
-
 sunshine_hours = np.array([11.70, 11.80, 12.00, 12.10, 12.31,
                            12.30, 12.30, 12.20, 12.00, 11.90, 11.69, 11.70])
 
 monthy_days = np.array(utils.number_of_day_per_month(2023))
-monthly_heat = monthly_heat_index(temp_celsius)
+monthly_heat = utils.monthly_heat_index(temp_celsius)
+
+
 anual_heat = np.sum(monthly_heat)
-anual_3rd_polynomial = third_degree_polynomial(anual_heat)
-ept_unadjust = ept_without_correction(
+anual_3rd_polynomial = utils.third_degree_polynomial(anual_heat)
+ept_unadjust = utils.ept_without_correction(
     temp_celsius, anual_heat, anual_3rd_polynomial)
-ept_adjusted = ept_correction(ept_unadjust, sunshine_hours, monthy_days)
+ept_adjusted = utils.ept_correction(ept_unadjust, sunshine_hours, monthy_days)
 ept_adjusted = np.append(ept_adjusted, np.sum(ept_adjusted))
 precipitation_mm = np.append(precipitation_mm, np.sum(precipitation_mm))
-tmi = thornthwaite_moisture_index(ept_adjusted, precipitation_mm)
+tmi = utils.thornthwaite_moisture_index(ept_adjusted, precipitation_mm)
 
-p_middle = sieves_passing['No200']
 
-(p_before, p_after) = sorround_num_keys(
-    TMI_PLASTIC.keys(), p_middle)
+if mode == 'thin':
+    a = 0.3
+    if p200 == 10 and wpi == 0:
+        b = TMI_NO_PLASTIC[0]['b']
+        y = TMI_NO_PLASTIC[0]['y']
+        s = TMI_NO_PLASTIC[0]['s']
 
-a_before = TMI_PLASTIC[p_before]['a']
-a_after = TMI_PLASTIC[p_after]['a']
+    if p200 == 50 and wpi == 0:
+        b = TMI_NO_PLASTIC[1]['b']
+        y = TMI_NO_PLASTIC[1]['y']
+        s = TMI_NO_PLASTIC[1]['s']
 
-a = lerp(int(p_before), a_before, int(p_after), a_after, p_middle)
+    if wpi <= 0.5:
+        b = TMI_NO_PLASTIC[2]['b']
+        y = TMI_NO_PLASTIC[2]['y']
+        s = TMI_NO_PLASTIC[2]['s']
 
-b_before = TMI_PLASTIC[p_before]['b']
-b_after = TMI_PLASTIC[p_after]['b']
+    if wpi >= 50:
+        b = TMI_NO_PLASTIC[6]['b']
+        y = TMI_NO_PLASTIC[6]['y']
+        s = TMI_NO_PLASTIC[6]['s']
 
-b = lerp(int(p_before), b_before, int(p_after), b_after, p_middle)
+    if wpi > 0.5 and wpi < 50:
+        tmi_table = [5, 10, 20]
 
-y_before = TMI_PLASTIC[p_before]['y']
-y_after = TMI_PLASTIC[p_after]['y']
+        p_middle = wpi
+        (p_before, p_after) = sorround_num_keys(
+            tmi_table, p_middle)
 
-y = lerp(int(p_before), y_before, int(p_after), y_after, p_middle)
+        p_before_i = tmi_table.index(p_before) + 3
+        p_after_i = tmi_table.index(p_after) + 3
 
-hm = matric_suction_no_plastic(a, b, y, tmi)
+        s_before = TMI_NO_PLASTIC[p_before_i]['s']
+        s_after = TMI_NO_PLASTIC[p_after_i]['s']
+
+        s = utils.lerp(int(p_before), s_before,
+                       int(p_after), s_after, p_middle)
+
+        b_before = TMI_NO_PLASTIC[p_before_i]['b']
+        b_after = TMI_NO_PLASTIC[p_after_i]['b']
+
+        b = utils.lerp(int(p_before), b_before,
+                       int(p_after), b_after, p_middle)
+
+        y_before = TMI_NO_PLASTIC[p_before_i]['y']
+        y_after = TMI_NO_PLASTIC[p_after_i]['y']
+
+        y = utils.lerp(int(p_before), y_before,
+                       int(p_after), y_after, p_middle)
+else:
+    p_middle = p200
+    (p_before, p_after) = sorround_num_keys(
+        TMI_PLASTIC.keys(), p_middle)
+
+    a_before = TMI_PLASTIC[p_before]['a']
+    a_after = TMI_PLASTIC[p_after]['a']
+
+    a = utils.lerp(int(p_before), a_before, int(p_after), a_after, p_middle)
+
+    b_before = TMI_PLASTIC[p_before]['b']
+    b_after = TMI_PLASTIC[p_after]['b']
+
+    b = utils.lerp(int(p_before), b_before, int(p_after), b_after, p_middle)
+
+    y_before = TMI_PLASTIC[p_before]['y']
+    y_after = TMI_PLASTIC[p_after]['y']
+
+    y = utils.lerp(int(p_before), y_before, int(p_after), y_after, p_middle)
+
+print(a, b, y, s, tmi)
+
+if mode == 'thin':
+    hm = utils.matric_suction_plastic(a, b, y, s, tmi)
+else:
+    hm = utils.matric_suction_no_plastic(a, b, y, tmi)
 
 # SWCC parameter
-hr = 100
-d90 = PARTICLE_SIZE_PASSING_PERCENT['D90']
-d60 = PARTICLE_SIZE_PASSING_PERCENT['D60']
-d20 = PARTICLE_SIZE_PASSING_PERCENT['D20']
-d30 = PARTICLE_SIZE_PASSING_PERCENT['D30']
-d10 = PARTICLE_SIZE_PASSING_PERCENT['D10']
-p200 = sieves_passing['No200']
-m1 = m_1(d90, d60)
-d100 = d_100(m1, d60)
-a2 = a_swcc(d20, p200, d30, d100)
-af = af_swcc(a2, p200)
+
+if mode == 'thin':
+    hr = 500
+else:
+    hr = 100
+
+ch = utils.adjust_factor(hm, hr)
+
+if mode == 'thin':
+    af = utils.af_swcc_thin(wpi)
+    bf = utils.bf_swcc_thin(wpi)
+    cf = utils.cf_swcc_thin(wpi)
+else:
+    d90 = PARTICLE_SIZE_PASSING_PERCENT['D90']
+    d60 = PARTICLE_SIZE_PASSING_PERCENT['D60']
+    d20 = PARTICLE_SIZE_PASSING_PERCENT['D20']
+    d30 = PARTICLE_SIZE_PASSING_PERCENT['D30']
+    d10 = PARTICLE_SIZE_PASSING_PERCENT['D10']
+    p200 = sieves_passing['No200']
+    m1 = utils.m_1(d90, d60)
+    d100 = utils.d_100(m1, d60)
+    a2 = utils.a_swcc(d20, p200, d30, d100)
+    af = utils.af_swcc(a2, p200)
+
+    m2 = utils.m_2(d30, d10)
+    d0 = utils.d_0(m2, d30)
+    b = utils.b_swcc(p200, d90, d10, d0, m1)
+    bf = utils.bf_swcc(b)
+
+    c = utils.c_swcc(m2, bf)
+    cf = utils.cf_swcc(c, d10)
 
 
-m2 = m_2(d30, d10)
-d0 = d_0(m2, d30)
-b = b_swcc(p200, d90, d10, d0, m1)
-bf = bf_swcc(b)
+osat = utils.volumetry_humedity_saturated(
+    maximum_dry_density, specific_gravity)
 
-c = c_swcc(m2, bf)
-cf = cf_swcc(c, d10)
-
-ch = adjust_factor(hm, hr)
-
-osat = volumetry_humedity_saturated(maximum_dry_density, specific_gravity)
-ow = volumetry_humedity(ch, osat, hm, af, bf, cf)
+ow = utils.volumetry_humedity(ch, osat, hm, af, bf, cf)
 
 s = ow / osat
 
-a3 = -0.3123
-b3 = 0.3
-km = 6.8157
+if mode == 'thin':
+    a3 = -0.5934
+    b3 = 0.4
+    km = 6.1324
+else:
+    a3 = -0.3123
+    b3 = 0.3
+    km = 6.8157
 
-tetha_opt = tetha_opt(optimum_moisture_content, maximum_dry_density)
+tetha_opt = utils.tetha_opt(optimum_moisture_content, maximum_dry_density)
 sopt = tetha_opt / osat
 
-famb = ambient_factor(a3, b3, km, s, sopt)
+famb = utils.ambient_factor(a3, b3, km, s, sopt)
 cbr = famb * california_bearing_ratio
-
-print(cbr)
-
-
-# assert (
-#    len(SIEVES_SIZES_IN_MM) == len(sieves_passing)
-# ), "Missing sieves passing elements."
--- utils.py
+++ utils.py
@@ -1,3 +1,7 @@
+import math
+import numpy as np
+
+
 def is_leap_year(year: int):
     return year % 4 == 0 or year % 100 == 0 and year % 400 == 0
 
@@ -17,3 +21,133 @@ def number_of_day_per_month(year):
         30,
         31
     ]
+
+
+def lerp(x0, y0, x1, y1, x):
+    return y0 + (x-x0)*(y1-y0)/(x1-x0)
+
+
+def ambient_factor(a3, b3, km, s, sopt):
+    return 10 ** (
+        a3 + (b3 - a3) / (1 + np.exp(np.log(-b3 / a3) + km * (s - sopt)))
+    )
+
+
+def tetha_opt(wopt, pdmax):
+    return ((wopt / 100) * pdmax)/1000
+
+
+def c_swcc(m2, bf):
+    return math.log10(m2**1.15) - (1 - (1 / bf))
+
+
+def cf_swcc_thin(wpi):
+    value = -0.2154 * math.log(wpi) + 0.7145
+    return 0.03 if value < 0.01 else value
+
+
+def bf_swcc_thin(wpi):
+    return 1.421 * wpi**(-0.3185)
+
+
+def af_swcc_thin(wpi):
+    value = 32.835 * math.log(wpi) + 32.438
+    return 5 if value < 5 else value
+
+
+def cf_swcc(c, d10):
+    return 0.26 * math.exp(0.758 * c) + 1.4 * d10
+
+
+def bf_swcc(b):
+    if 0.936 * b - 3.8 < 0.3:
+        return 0.3
+    elif 0.936 * b - 3.8 >= 4:
+        return 4
+    else:
+        return 0.936 * b - 3.8
+
+
+def af_swcc(a, p200):
+    if (1.14 * a - 0.5) < 0:
+        return 1
+    elif (1.14 * a - 0.5) > 0 and (1.14 * a - 0.5) < 1:
+        return 2.25 * p200**0.5 + 5
+    else:
+        return 1.14 * a - 0.5
+
+
+def b_swcc(p200, d90, d10, d0, m1):
+    return (5.39 - 0.29 * math.log(
+        p200 * (d90 / d10)
+    ) + 3 * d0**0.57 + 0.021 * p200**1.19) * m1**0.1
+
+
+def a_swcc(d20, p200, d30, d100):
+    return -2.79 - 14.1 * math.log10(d20) - 1.9 * 10**-6 *\
+        p200**4.34 + 7 * math.log10(d30) + 0.055 * d100
+
+
+def d_100(m1, d60):
+    return 10 ** ((40/m1) + math.log10(d60))
+
+
+def d_0(m2, d30):
+    return 10 ** ((-30/m2) + math.log10(d30))
+
+
+def m_1(d90, d60):
+    return 30 / (math.log10(d90) - math.log10(d60))
+
+
+def m_2(d30, d10):
+    return 20 / (math.log10(d30) - math.log10(d10))
+
+
+def volumetry_humedity_saturated(pdmax, gs):
+    return 1 - pdmax / (1000*gs)
+
+
+def volumetry_humedity(ch, osat, hm, af, bf, cf):
+    return ch * (osat/(np.log(np.exp(1) + (hm/af) ** bf)**cf))
+
+
+def monthly_heat_index(average_temperature_celsius):
+    return (average_temperature_celsius/5) ** 1.514
+
+
+def ept_without_correction(
+    temp_celsius,
+    anual_heat,
+    anual_temp_3rd_poly
+):
+    return 16 * (10 * temp_celsius / anual_heat) ** anual_temp_3rd_poly
+
+
+def ept_correction(ept_unadjust, sunshine_hours, monthly_days):
+    return ept_unadjust * (sunshine_hours/12)*(monthly_days/30)
+
+
+def thornthwaite_moisture_index(ept_adjusted, precipitation,):
+    return 75 * (precipitation / ept_adjusted - 1) + 10
+
+
+def matric_suction_plastic(a, b, y, s, tmi):
+    return a * (np.exp(b / (tmi + y)) + s)
+
+
+def matric_suction_no_plastic(a, b, y, tmi):
+    return a + math.e ** (b+y*(tmi+101))
+
+
+def third_degree_polynomial(anual_temp):
+    a = (675 * 10**-9 * anual_temp ** 3)
+    b = (771 * 10**-7 * anual_temp ** 2)
+    c = (1792 * 10**-5 * anual_temp)
+    d = 0.49239
+
+    return a - b + c + d
+
+
+def adjust_factor(hm, hr):
+    return (1 - (np.log(1 + (hm / hr)) / (np.log(1 + (10 ** 6 / hr)))))
