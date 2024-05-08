eb171adf1ca00e6ff14801a8b7581db2acfc4400
camilo-camargo <camilocamargo49@gmail.com>
2024-05-08 17:37:22 -0500

    feat: add production compilation script

---
+++ production.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PWD=$(pwd)
+PRODUCTION_FILE_NAME="production"
+PRODUCTION_DIR=$PWD/$PRODUCTION_FILE_NAME
+CORE_DIR=$PWD/core
+WEB_DIR=$PWD/web-spa
+CORE_OUT_DIR=core
+
+mkdir $PRODUCTION_DIR
+mkdir $PRODUCTION_DIR/$CORE_OUT_DIR
+
+pushd $WEB_DIR
+  yarn install
+  yarn run build
+  mv build/client $PRODUCTION_DIR/$CORE_OUT_DIR/public
+popd
+
+pushd $CORE_DIR
+  cp -r * $PRODUCTION_DIR/$CORE_OUT_DIR
+popd
+
+cp compose.yml $PRODUCTION_DIR
